1628a507726deae2a8064efbcec53ae0d896f5c1
cecileNyanou <cecile.nyanou@cagecfi.com>
2024-01-10 18:25:26 +0000

    second

--- src/main/java/com/example/demoMa/DemoMaApplication.java
+++ src/main/java/com/example/demoMa/DemoMaApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.demoMa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoMaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DemoMaApplication.class, args);
 	}
 
 }
